32f0a6af60e5e9c6d1eeaf3e6ed0791d3d2ef34d
zalviandyr <zukronalviandy@gmail.com>
2022-06-13 22:38:57 +0700

    fix: asset path

--- lib/src/constants/asset_icons.dart
+++ lib/src/constants/asset_icons.dart
@@ -1,13 +1,15 @@
 class AssetIcons {
-  static const String google = 'assets/icons/google.svg';
-  static const String eye = 'assets/icons/eye.svg';
-  static const String eyeClosed = 'assets/icons/eye_closed.svg';
-  static const String circleCheck = 'assets/icons/circle_check.svg';
-  static const String cartFilled = 'assets/icons/cart_filled.svg';
-  static const String cartRegular = 'assets/icons/cart_regular.svg';
+  static const String _path = 'packages/mari_pesan_sdk/assets';
 
-  static const String statusSuccess = 'assets/icons/status_success.svg';
-  static const String statusFailed = 'assets/icons/status_failed.svg';
-  static const String statusProceed = 'assets/icons/status_proceed.svg';
-  static const String statusWarning = 'assets/icons/status_warning.svg';
+  static const String google = '$_path/icons/google.svg';
+  static const String eye = '$_path/icons/eye.svg';
+  static const String eyeClosed = '$_path/icons/eye_closed.svg';
+  static const String circleCheck = '$_path/icons/circle_check.svg';
+  static const String cartFilled = '$_path/icons/cart_filled.svg';
+  static const String cartRegular = '$_path/icons/cart_regular.svg';
+
+  static const String statusSuccess = '$_path/icons/status_success.svg';
+  static const String statusFailed = '$_path/icons/status_failed.svg';
+  static const String statusProceed = '$_path/icons/status_proceed.svg';
+  static const String statusWarning = '$_path/icons/status_warning.svg';
 }
--- lib/src/constants/asset_images.dart
+++ lib/src/constants/asset_images.dart
@@ -1,30 +1,32 @@
 class AssetImages {
-  static const String onboarding1 = 'assets/images/onboarding_1.png';
-  static const String onboarding2 = 'assets/images/onboarding_2.png';
-  static const String onboarding3 = 'assets/images/onboarding_3.png';
+  static const String _path = 'packages/mari_pesan_sdk/assets';
 
-  static const String user = 'assets/images/user.png';
+  static const String onboarding1 = '$_path/images/onboarding_1.png';
+  static const String onboarding2 = '$_path/images/onboarding_2.png';
+  static const String onboarding3 = '$_path/images/onboarding_3.png';
 
-  static const String pizza = 'assets/images/pizza.jpg';
-  static const String coffee = 'assets/images/coffee.jpg';
-  static const String localFood = 'assets/images/local_food.jpg';
-  static const String pasta = 'assets/images/pasta.jpg';
-  static const String chicken = 'assets/images/chicken.jpg';
-  static const String beef = 'assets/images/beef.jpg';
-  static const String seafood = 'assets/images/seafood.jpg';
-  static const String beverages = 'assets/images/beverages.jpg';
+  static const String user = '$_path/images/user.png';
 
-  static const String noImage = 'assets/images/no_image.png';
-  static const String noImage2 = 'assets/images/no_image_2.png';
+  static const String pizza = '$_path/images/pizza.jpg';
+  static const String coffee = '$_path/images/coffee.jpg';
+  static const String localFood = '$_path/images/local_food.jpg';
+  static const String pasta = '$_path/images/pasta.jpg';
+  static const String chicken = '$_path/images/chicken.jpg';
+  static const String beef = '$_path/images/beef.jpg';
+  static const String seafood = '$_path/images/seafood.jpg';
+  static const String beverages = '$_path/images/beverages.jpg';
 
-  static const String dana = 'assets/images/dana.png';
-  static const String gopay = 'assets/images/gopay.png';
-  static const String linkAja = 'assets/images/link_aja.png';
-  static const String ovo = 'assets/images/ovo.png';
-  static const String shopeePay = 'assets/images/shopee_pay.png';
+  static const String noImage = '$_path/images/no_image.png';
+  static const String noImage2 = '$_path/images/no_image_2.png';
 
-  static const String emptyFavorite = 'assets/images/empty_favorite.png';
-  static const String emptySearch = 'assets/images/empty_search.png';
-  static const String emptyOrder = 'assets/images/empty_order.png';
-  static const String invalidCart = 'assets/images/invalid_cart.png';
+  static const String dana = '$_path/images/dana.png';
+  static const String gopay = '$_path/images/gopay.png';
+  static const String linkAja = '$_path/images/link_aja.png';
+  static const String ovo = '$_path/images/ovo.png';
+  static const String shopeePay = '$_path/images/shopee_pay.png';
+
+  static const String emptyFavorite = '$_path/images/empty_favorite.png';
+  static const String emptySearch = '$_path/images/empty_search.png';
+  static const String emptyOrder = '$_path/images/empty_order.png';
+  static const String invalidCart = '$_path/images/invalid_cart.png';
 }
